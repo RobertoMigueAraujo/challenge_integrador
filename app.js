bcd4c5971d04a044af3596c7ceb60ed767c1dfe6
Fernando <duartef@gmail.com>
2023-12-03 15:24:17 -0300

    Modificaciones de css shop, header y unificar mission 4

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
+/*configurar servidor*/
 const express = require('express');
 const app = express();
-const routes = require('./routes/index');
-const shopRoutes = require('./routes/shop');
-const adminRoutes = require('./routes/admin');
-const authRoutes = require('./routes/auth');
+const mainRoutes = require('./src/routes/mainRoutes');
+const shopRoutes = require('./src/routes/shopRoutes');
+const adminRoutes = require('./src/routes/adminRoutes');
+const authRoutes = require('./src/routes/authRoutes');
 
 app.use(express.static('public'));
-app.use('/', routes);
+
+app.use('/', mainRoutes);
 app.use('/shop', shopRoutes);
 app.use('/admin', adminRoutes);
 app.use('/auth', authRoutes);
 
-app.listen(3000, () => {
-    console.log('El servidor está corriendo en http://localhost:3000');
-});
+app.listen(3000, () => console.log("Servidor corriendo en http://localhost:3000"));
