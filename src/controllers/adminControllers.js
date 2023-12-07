5bfe52f275f1e99279aa2a7ded9cdc7e4cdf28f6
mouny2023 <zfatima766@gmail.com>
2023-12-06 23:35:04 -0300

    falta validar formulario

--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -9,29 +9,25 @@ const adminControllers = {
 
 
   edit: (req, res) => {
-    const productId = req.params.id; // Obtener el ID del producto de los parámetros de la solicitud
-    // Aquí podrías obtener los detalles del producto correspondiente a productId de tu base de datos o alguna fuente de datos simulada
-    const productDetails = getProductDetails(productId); // Función hipotética para obtener detalles del producto
+    const productId = req.params.id;  
+    const productDetails = getProductDetails(productId); 
 
     if (!productDetails) {
-      // Manejar el caso en el que el producto no existe
+     
       return res.status(404).send('Producto no encontrado');
     }
 
-    // Renderizar la página de edición del producto con los detalles obtenidos
-    res.render('admin/edit', { producto: productDetails });
+    res.rende ('admin/edit', { producto: productDetails });
   },
 
   remove: (req, res) => {
-    const productId = req.params.id; // Obtener el ID del producto de los parámetros de la solicitud
-    // Aquí podrías realizar la lógica para eliminar el producto correspondiente a productId de tu base de datos o fuente de datos simulada
-    deleteProduct(productId); // Función hipotética para eliminar el producto
+    const productId = req.params.id; 
+    deleteProduct(productId); 
 
-    // Redirigir a la página principal de administración después de eliminar el producto
-    res.redirect('/admin');
+    res.redirect ('/admin');
   },
 
-  // ... otras funciones de controlador
+ 
 };
 
 module.exports = adminControllers;
--- src/controllers/shopControllers.js
+++ src/controllers/shopControllers.js
@@ -4,20 +4,16 @@ const shopControllers = {
   },
   getItemById: (req, res) => {
     const itemId = req.params.id;
-    // Lógica para obtener el item con el ID proporcionado y luego renderizar la vista específica
     res.render('shop/item/viewItem', { itemId: itemId });
   },
   addItemToCart: (req, res) => {
     const itemId = req.params.id;
-    // Lógica para agregar el item al carrito y luego renderizar la vista del carrito actualizado
     res.render('shop/cart/addToCart', { itemId: itemId });
   },
   getCart: (req, res) => {
-    // Lógica para obtener el carrito actual y luego renderizar la vista del carrito
     res.render('shop/cart/viewCart', { title: 'Ver carrito' });
   },
   addToCart: (req, res) => {
-    // Lógica para agregar un item al carrito y luego renderizar la vista del carrito actualizado
     res.render('shop/cart/addToCart', { title: 'Añadir al carrito' });
   }
 };
